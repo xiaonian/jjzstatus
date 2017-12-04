d960565b7c195a259bc4252851d98f7c12a78b74
张念 <520zhangnian@gmail.com>
2017-12-04 10:32:36 +0800

    加入错误处理功能

--- jinjing.py
+++ jinjing.py
@@ -19,15 +19,18 @@ def get_status():
 if __name__ == "__main__":
     corpid = '企业CorpID'
     corpsecret = '应用Secret'
-	agentid = "应用AgentID"
-    laststatus = 1
+    agentid = "应用AgentID"
+    laststatus = 0
     while True:
-        status = get_status()
-        if status != laststatus:
-            # 状态发生改变时告警
-            laststatus = status
-            message = "进京证办理通道开启" if status == 1 else "进京证办理通道关闭"
+	try:
+	    status = get_status()
+	    if status != laststatus:
+	        # 状态发生改变时告警
+	        laststatus = status
+	        message = "进京证办理通道开启" if status == 1 else "进京证办理通道关闭"
 			#以下是企业微信告警推送
-            accesstoken = gettoken(corpid, corpsecret)
-            senddata(accesstoken, message, 4, None, agentid)
+	        accesstoken = gettoken(corpid, corpsecret)
+	        senddata(accesstoken, message, 4, None, agentid)
+	except Exception,e:
+            print Exception,e
         sleep(60)
